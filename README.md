309e9083d3f5f863853d80c82a9c1f47dce95199
Gabor Papp <gabor.papp@gmail.com>
2012-11-13 12:06:10 +0100

    osx scons build

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ Cinder-OpenSSL
 This is the Crypter CinderBlock, designed for use with the open-source C++ library Cinder: http://libcinder.org
 
 Integrates OpenSSL 1.0.1c
+
+Generating RSA keypair
+======================
+openssl genrsa -des3 -out private.pem 2048
+openssl rsa -in private.pem -pubout -out public.pem
--- samples/CrypterTest/src/CrypterTest.cpp
+++ samples/CrypterTest/src/CrypterTest.cpp
@@ -5,7 +5,7 @@ using namespace mndl::crypter;
 using namespace std;
 using namespace ci;
 
-void main()
+int main()
 {
 	string text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
@@ -34,4 +34,6 @@ void main()
 		else
 			cout << "private to public unsuccessful" << endl;
 	}
+
+	return 0;
 }
